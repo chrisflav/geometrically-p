2d9a684b91d93b9a6202238b68990e575fc0ca7a
judithludwig <150042763+judithludwig@users.noreply.github.com>
2025-12-01 12:31:28 +0100

    fill sorry in Connected (#19)
    I filled the sorry of
    `PrimeSpectrum.preconnectedSpace_of_forall_connectedSpace_of_isSeparable`

--- GeometricallyP/Algebra/Connected.lean
+++ GeometricallyP/Algebra/Connected.lean
@@ -21,6 +21,9 @@ variable {k : Type u} {R : Type*} [Field k] [CommRing R] [Algebra k R]
 abbrev idempotents (R : Type*) [Mul R] : Set R :=
   { e | IsIdempotentElem e}
 
+lemma subset_idempotents : {0, 1} ⊆ idempotents R := by
+  simp [idempotents, Set.subset_def, IsIdempotentElem.zero, IsIdempotentElem.one]
+
 /-- If every idempotent is trivial, then `Spec R` is connected. -/
 lemma PrimeSpectrum.preconnectedSpace_of_forall_isIdempotentElem
     (H : ∀ e : R, IsIdempotentElem e → e = 0 ∨ e = 1) :
@@ -41,51 +44,40 @@ lemma PrimeSpectrum.basicOpen_eq_top_iff (f : R) : basicOpen f = ⊤ ↔ IsUnit 
   · rw [← PrimeSpectrum.zeroLocus_span {f}]
     simp [PrimeSpectrum.zeroLocus_empty_of_one_mem, Ideal.span_singleton_eq_top.mpr x]
 
-lemma PrimeSpectrum.preconnectedSpace_iff_idempotents_eq :
-    PreconnectedSpace (PrimeSpectrum R) ↔ idempotents R = {0, 1} := by
+lemma PrimeSpectrum.preconnectedSpace_iff_idempotents_subset :
+    PreconnectedSpace (PrimeSpectrum R) ↔ idempotents R ⊆ {0, 1} := by
   refine ⟨fun h ↦ ?_, fun h ↦ ?_⟩
   · simp_rw [preconnectedSpace_iff_clopen, PrimeSpectrum.isClopen_iff] at h
-    refine subset_antisymm (fun e he ↦ ?_) ?_
-    · have he1 : IsIdempotentElem e := by simpa
-      obtain h1 | h2 := h (PrimeSpectrum.basicOpen e) ⟨e, he, rfl⟩
-      · obtain rfl : e = 0 := by
-          rw [← Set.bot_eq_empty] at h1
-          have h2 : IsNilpotent e := by
-            apply (PrimeSpectrum.basicOpen_eq_bot_iff e).mp
-            ext1
-            exact h1
-          exact IsNilpotent.eq_zero_of_isIdempotentElem he1 h2
-        simp
-      · obtain rfl : e = 1 := by
-          rw [← Set.top_eq_univ] at h2
-          have h3 : IsUnit e := by
-            apply (PrimeSpectrum.basicOpen_eq_top_iff e).mp
-            ext1
-            exact h2
-          exact (IsUnit.mul_eq_left h3).mp he
-        simp
-    · intro e
-      simp only [Set.mem_insert_iff, Set.mem_singleton_iff, Set.mem_setOf_eq]
-      intro h
-      obtain h1|h2 := h
-      · rw [h1]
-        exact IsIdempotentElem.zero
-      · rw [h2]
-        exact IsIdempotentElem.one
+    refine fun e he ↦ ?_
+    have he1 : IsIdempotentElem e := by simpa
+    obtain h1 | h2 := h (PrimeSpectrum.basicOpen e) ⟨e, he, rfl⟩
+    · obtain rfl : e = 0 := by
+        rw [← Set.bot_eq_empty] at h1
+        have h2 : IsNilpotent e := by
+          apply (PrimeSpectrum.basicOpen_eq_bot_iff e).mp
+          ext1
+          exact h1
+        exact IsNilpotent.eq_zero_of_isIdempotentElem he1 h2
+      simp
+    · obtain rfl : e = 1 := by
+        rw [← Set.top_eq_univ] at h2
+        have h3 : IsUnit e := by
+          apply (PrimeSpectrum.basicOpen_eq_top_iff e).mp
+          ext1
+          exact h2
+        exact (IsUnit.mul_eq_left h3).mp he
+      simp
   · apply PrimeSpectrum.preconnectedSpace_of_forall_isIdempotentElem
     intro e he
-    have : e ∈ ({0,1} : Set R) := by rwa [← h]
-    simpa using this
+    exact h he
 
-lemma PrimeSpectrum.connectedSpace_of_forall_connectedSpace_of_isSeparable
+lemma PrimeSpectrum.preconnectedSpace_of_forall_connectedSpace_of_isSeparable
     (H : ∀ (K : Type v) [Field K] [Algebra k K] [Module.Finite k K] [Algebra.IsSeparable k K],
-      ConnectedSpace (PrimeSpectrum (K ⊗[k] R)))
+      PreconnectedSpace (PrimeSpectrum (K ⊗[k] R)))
     (Ω : Type v) [Field Ω] [Algebra k Ω] [Algebra.IsSeparable k Ω] :
-    ConnectedSpace (PrimeSpectrum (Ω ⊗[k] R)) :=
-  /-
-  Use `eq_zero_or_eq_one_of_isIdempotentElem_of_forall_isSeparable`
-  -/
-  sorry
+    PreconnectedSpace (PrimeSpectrum (Ω ⊗[k] R)) := by
+  simp_rw [PrimeSpectrum.preconnectedSpace_iff_idempotents_subset] at H ⊢
+  exact eq_zero_or_eq_one_of_isIdempotentElem_of_forall_isSeparable H _
 
 @[stacks 037R]
 lemma PrimeSpectrum.connectedSpace_tensorProduct_of_isSepClosed [IsSepClosed k] {S : Type*}
