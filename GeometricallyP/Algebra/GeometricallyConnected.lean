094a22d295b009a28399a323f2c67fe650b004d8
Christian Merten <christian@merten.dev>
2026-01-19 14:14:57 +0100

    Update GeometricallyP/Algebra/GeometricallyConnected.lean

--- GeometricallyP/Algebra/GeometricallyConnected.lean
+++ GeometricallyP/Algebra/GeometricallyConnected.lean
@@ -44,8 +44,8 @@ variable (k : Type u) (R : Type*) [CommRing R] [Field k] [Algebra k R]
 lemma iff_connectedSpace_separableClosure :
     GeometricallyConnected k R ↔
       ConnectedSpace (PrimeSpectrum (SeparableClosure k ⊗[k] R)) := by
-      rw [geometricallyConnected_iff]
-      exact (PrimeSpectrum.connectedSpace_iff_of_isAlgClosure_of_isSepClosure _ _ _ _).symm
+    rw [geometricallyConnected_iff]
+    exact (PrimeSpectrum.connectedSpace_iff_of_isAlgClosure_of_isSepClosure _ _ _ _).symm
 
 /-- If `Spec (K ⊗[k] R)` is connected for every finite separable extension `K` of `k`, then
 `R` is geometrically connected over `k`. -/
